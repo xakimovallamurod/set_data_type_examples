f3f16a6628cf32a735387a6c124b4601046b6d4b
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2025-02-12 20:31:30 +0200

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,59 @@
+import csv
+
+def read_csv(file_path):
+    # Read and parse the CSV file
+        
+    # Return a readDict object
+    return
+
+
+def first_name_all(data):
+
+    # Users data parse
+
+    # Return all unique first names from the data.
+    
+    return
+
+def last_name_all(data):
+
+    # Users data parse
+
+    # Return all unique last names from the data.
+    
+    return
+
+def gender_male_count(data):
+
+    # Users data parse
+
+    # retrun gender mael count 
+    
+    return
+
+def gender_female_count(data):
+
+    # Users data parse
+
+    # retrun gender femael count 
+    
+    return
+
+def get_all_domen_name(data):
+
+    # Users data parse
+
+    # Return all unique domain names from the data.
+
+    # sample: username@gmail.com -> domen_name = 'gmail'
+    
+    return 
+
+def get_job(data):
+
+    # Return all unique jobs name from the data.
+
+    # Return a list of entries with the same job
+
+    return
+
